robinkrahl/LrMediaWiki

commit 8862341672dc288596d6ef66f92751bba6dfef25

Fix coding style and add `luacheck` to Travis CI.
Fix #40.

mediawiki.lrdevplugin/MediaWikiApi.lua
@@ -36,7 +36,7 @@ function MediaWikiApi.urlEncode(str)
 			function(c) return string.format('%%%02X', string.byte(c)) end)
 		str = string.gsub(str, ' ', '+')
 	end
-	return str	
+	return str
 end
 
 --- Convert HTTP arguments to a URL-encoded request body.
@@ -64,7 +64,7 @@ function MediaWikiApi.parseXmlDom(xmlDomInstance)
 		end
 		for i = 1, xmlDomInstance:childCount() do
 			local child = xmlDomInstance:childAtIndex(i)
-			local childName, childNamespace = child:name()
+			local childName = child:name()[0]
 			if childName then
 				value[childName] = MediaWikiApi.parseXmlDom(child)
 			end
@@ -88,7 +88,7 @@ function MediaWikiApi.performRequest(arguments)
 			value = 'application/x-www-form-urlencoded',
 		},
 	}
-	
+
 	local resultBody, resultHeaders = LrHttp.post(MediaWikiApi.apiPath, requestBody, requestHeaders)
 
 	if not resultHeaders.status then
@@ -114,14 +114,14 @@ function MediaWikiApi.login(username, password, token)
 		arguments.lgtoken = token
 	end
 	local xml = MediaWikiApi.performRequest(arguments)
-	
+
 	local loginResult = xml.login.result
 	if loginResult == 'Success' then
 		return true
 	elseif not token and loginResult == 'NeedToken' then
 		return MediaWikiApi.login(username, password, xml.login.token)
 	end
-	
+
 	return loginResult
 end
 
@@ -175,30 +175,30 @@ function MediaWikiApi.upload(fileName, sourceFilePath, text, comment, ignoreWarn
 		filePath = sourceFilePath,
 		contentType = 'application/octet-stream',
 	}
-	
+
 	local resultBody, resultHeaders = LrHttp.postMultipart(MediaWikiApi.apiPath, requestBody, requestHeaders)
-	
+
 	if resultHeaders.status ~= 200 then
 		LrErrors.throwUserError(LOC('$$$/LrMediaWiki/Api/HttpError=Received HTTP status ^1.', resultHeaders.status))
 	end
-       
+
 	local resultXml = MediaWikiApi.parseXmlDom(LrXml.parseXml(resultBody))
 	if resultXml.error then
 		LrErrors.throwUserError(LOC('$$$/LrMediaWiki/Api/MediaWikiError=The MediaWiki error ^1 occured: ^2', resultXml.error.code, resultXml.error.info))
 	end
-	
-	local uploadResult = resultXml.upload.result 
+
+	local uploadResult = resultXml.upload.result
 	if uploadResult == 'Success' then
 		return true
 	elseif uploadResult == 'Warning' then
-		warnings = ''
-		for k, v in pairs(resultXml.upload.warnings) do
+		local warnings = ''
+		for pair in pairs(resultXml.upload.warnings) do
 			if warnings ~= '' then
 				warnings = warnings .. ', '
 			end
-			warnings = warnings .. k
+			warnings = warnings .. pair[0]
 		end
-                return warnings
+    return warnings
 	else
 		return uploadResult
 	end

mediawiki.lrdevplugin/MediaWikiExportServiceProvider.lua
@@ -31,7 +31,7 @@ MediaWikiExportServiceProvider.processRenderedPhotos = function(functionContext,
 	-- configure progess display
 	local exportSession = exportContext.exportSession
 	local photoCount = exportSession:countRenditions()
-	local progressScope = exportContext:configureProgress{
+	exportContext:configureProgress{
 		title = photoCount > 1 and LOC('$$$/LrMediaWiki/Export/Progress=Exporting ^1 photos to a MediaWiki', photoCount) or LOC '$$$/LrMediaWiki/Export/Progress/One=Exporting one photo to a MediaWiki',
 	}
 
@@ -60,7 +60,8 @@ MediaWikiExportServiceProvider.processRenderedPhotos = function(functionContext,
 	MediaWikiInterface.prepareUpload(exportSettings.username, exportSettings.password, exportSettings.api_path)
 
 	-- iterate over photos
-	for i, rendition in exportContext:renditions() do
+	for item in exportContext:renditions() do
+		local rendition = item[1]
 		-- render photo
 		local success, pathOrMessage = rendition:waitForRender()
 		if success then
@@ -291,7 +292,7 @@ MediaWikiExportServiceProvider.sectionsForTopOfDialog = function(viewFactory, pr
 				viewFactory:push_button {
 					title = LOC '$$$/LrMediaWiki/Section/Licensing/Preview=Preview generated wikitext',
 					action = function(button)
-						result, message = MediaWikiInterface.loadFileDescriptionTemplate()
+						local result, message = MediaWikiInterface.loadFileDescriptionTemplate()
 						if result then
 							local wikitext = MediaWikiInterface.buildFileDescription('<!-- description -->', propertyTable.info_source, '<!-- date -->', propertyTable.info_author, propertyTable.info_license, '<!-- {{Location}} if GPS metadata is available -->\n' .. propertyTable.info_templates, propertyTable.info_other, propertyTable.info_categories, '<!-- per-file categories -->', '<!-- permission -->')
 							LrDialogs.message(LOC '$$$/LrMediaWiki/Section/Licensing/Preview=Preview generated wikitext', wikitext, 'info')

.travis.yml
@@ -1,7 +1,9 @@
 language: c
 
 install:
-  - sudo apt-get install lua5.2
+  - sudo apt-get install lua5.2 luarocks
+  - sudo luarocks install luacheck
 
 script:
   - luac -p mediawiki.lrdevplugin/*.lua
+  - luacheck mediawiki.lrdevplugin/*.lua --globals LOC import _PLUGIN -a

mediawiki.lrdevplugin/MediaWikiInterface.lua
@@ -62,7 +62,7 @@ MediaWikiInterface.prepareUpload = function(username, password, apiPath)
 		LrErrors.throwUserError(LOC '$$$/LrMediaWiki/Interface/UsernameOrPasswordMissing=Username or password missing')
 	end
 	-- file description
-	result, message = MediaWikiInterface.loadFileDescriptionTemplate()
+	local result, message = MediaWikiInterface.loadFileDescriptionTemplate()
 	if not result then
 		LrErrors.throwUserError(message)
 	end

commit d3ba117b246a5e6e21c93b94cd40ae460d2d3541

Add logging support and fix refactoring errors.
The user now may enable logging in MediaWikiUtils. If logging is enabled, a
log file is written to the user’s "My Documents" that contains all API requests.
Furthermore, bugs introduced by the refactoring in commit
8862341672dc288596d6ef66f92751bba6dfef25 have been fixed.

 - set up logging in MediaWikiUtils
 - write to the log in MediaWikiApi.performRequest
 - fix error in MediaWikiExportServiceProvider.processRenderedPhotos
 - fix error in MediaWikiApi.parseXmlDom

Fix #43.

mediawiki.lrdevplugin/MediaWikiApi.lua
@@ -19,6 +19,7 @@ local LrPathUtils = import 'LrPathUtils'
 local LrXml = import 'LrXml'
 
 local Info = require 'Info'
+local MediaWikiUtils = require 'MediaWikiUtils'
 
 local MediaWikiApi = {
 	userAgent = string.format('LrMediaWiki %d.%d', Info.VERSION.major, Info.VERSION.minor),
@@ -64,7 +65,7 @@ function MediaWikiApi.parseXmlDom(xmlDomInstance)
 		end
 		for i = 1, xmlDomInstance:childCount() do
 			local child = xmlDomInstance:childAtIndex(i)
-			local childName = child:name()[0]
+			local childName = child:name()
 			if childName then
 				value[childName] = MediaWikiApi.parseXmlDom(child)
 			end
@@ -88,14 +89,23 @@ function MediaWikiApi.performRequest(arguments)
 			value = 'application/x-www-form-urlencoded',
 		},
 	}
+	MediaWikiUtils.trace('Performing API request');
+	MediaWikiUtils.trace('Request body:');
+	MediaWikiUtils.trace(requestBody);
 
 	local resultBody, resultHeaders = LrHttp.post(MediaWikiApi.apiPath, requestBody, requestHeaders)
+
+	MediaWikiUtils.trace('Result status:');
+	MediaWikiUtils.trace(resultHeaders.status);
 
 	if not resultHeaders.status then
 		LrErrors.throwUserError(LOC('$$$/LrMediaWiki/Api/NoConnection=Cannot connect to the MediaWiki API.'))
 	elseif resultHeaders.status ~= 200 then
 		LrErrors.throwUserError(LOC('$$$/LrMediaWiki/Api/HttpError=Received HTTP status ^1.', resultHeaders.status))
 	end
+
+	MediaWikiUtils.trace('Result body:');
+	MediaWikiUtils.trace(resultBody);
 
 	local resultXml = MediaWikiApi.parseXmlDom(LrXml.parseXml(resultBody))
 	if resultXml.error then

mediawiki.lrdevplugin/MediaWikiExportServiceProvider.lua
@@ -60,8 +60,7 @@ MediaWikiExportServiceProvider.processRenderedPhotos = function(functionContext,
 	MediaWikiInterface.prepareUpload(exportSettings.username, exportSettings.password, exportSettings.api_path)
 
 	-- iterate over photos
-	for item in exportContext:renditions() do
-		local rendition = item[1]
+	for i, rendition in exportContext:renditions() do
 		-- render photo
 		local success, pathOrMessage = rendition:waitForRender()
 		if success then

mediawiki.lrdevplugin/MediaWikiUtils.lua
@@ -13,7 +13,16 @@
 -- doc:   missing
 -- i18n:  complete
 
+local LrLogger = import 'LrLogger'
 local Info = require 'Info'
+local myLogger = LrLogger('LrMediaWikiLogger')
+
+-- LOGGING
+-- If enabled, the log file will appear in your "My Documents" folder. Warning:
+-- LrMediaWiki will log all requests sent to MediaWiki, including your password!
+-- If you share a log file, make sure you removed your password.
+-- To enable logging, uncomment the following line:
+-- myLogger:enable("logfile")
 
 local MediaWikiUtils = {}
 
@@ -34,6 +43,10 @@ MediaWikiUtils.getVersionString = function()
         str = str .. '.' .. Info.VERSION.revision
     end
     return str
+end
+
+MediaWikiUtils.trace = function(message)
+	myLogger:trace(message)
 end
 
 return MediaWikiUtils